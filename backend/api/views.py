b5f35ef7ae2d1bbcbf46bceaff0cc02a9eb986eb
David Xiong <xiongdsat@gmail.com>
2022-07-20 03:23:24 -0700

    Added html parsing functions and modified views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,6 @@
 import json
 from django.http import JsonResponse
-
+from .parser import *
 
 def api_home(request, *args, **kwargs):
 
@@ -14,7 +14,23 @@ def api_home(request, *args, **kwargs):
 
     print(data)
     data['params'] = dict(request.GET)
-    print(data['params']['url'][0])
+    # url = data['params']['url'][0]
+    url = 'https://www.popsci.com/science/omicron-coronavirus-variant/'
+    page_content = get_html(url)
+    processed_text = getArticleTextSections(page_content)
+    article_section = processArticleSections(processed_text)
+
+    quotes = getQuotes(processed_text)
+
+    title, author, date, image = getArticleInfo(page_content)
+
+    people, people_extended = getNamedEntities(processed_text)
+    attributed_quotes = attribute_quote(people_extended, quotes)
+
+    
+    print(url)
+    print(title, author,date,image)
+    print(attributed_quotes)
     # data['headers'] = dict(request.headers)
     # data['content_type'] = request.content_type
     return JsonResponse(data)

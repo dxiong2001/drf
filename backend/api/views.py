b63d743016858221bda11f4c26fbfcaa78d848a6
David Xiong <xiongdsat@gmail.com>
2022-07-19 22:30:26 -0700

    Added param parsing functionality

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,12 +8,13 @@ def api_home(request, *args, **kwargs):
 
     data = {}
     try:
-        data = json.loads(body) # strong of JSON data -> python dict
+        data = json.loads(body) # string of JSON data -> python dict
     except:
         pass
 
     print(data)
     data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    print(data['params']['url'][0])
+    # data['headers'] = dict(request.headers)
+    # data['content_type'] = request.content_type
     return JsonResponse(data)
--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%atlx%d(z#w^g&1+nq!q5ba5izvjb*k61q5t684dfjm#i%&x*l'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'api',
 ]
 
